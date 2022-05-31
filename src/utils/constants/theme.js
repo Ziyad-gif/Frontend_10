02a3331795d1ad508e4e5471d34b8fd549de52fa
Your Name <you@example.com>
2022-05-31 07:58:01 +0700

    done

--- src/utils/constants/theme.js
+++ src/utils/constants/theme.js
@@ -1,11 +1,28 @@
 const theme = {
-  // COLORS THEME
+  // Colors theme
   colors: {
-    primary: "#4361ee",
-    secondary: "#b5179e",
+      primary: '#4361ee',
+      secondary: "#b5179e"
   },
 
-  // Others: padding, margin, size
+  // Size Theme
+  sizes: {
+      font_size: {
+          sm:  0.8,
+          md: 1,
+          lg: 1.3
+      },
+      padding_y: {
+          sm: 0.2,
+          md: 0.5,
+          lg: 0.5
+      },
+      padding_x: {
+          sm: 0.5,
+          md: 1,
+          lg: 1
+      }
+  }
 };
 
 export default theme;
