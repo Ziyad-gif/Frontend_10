3d0c5f138e8e20ce4936f40a956afbc3f4d42929
Your Name <you@example.com>
2022-05-31 08:35:58 +0700

    done

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,52 +1,35 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import styles from "./Hero.module.css";
 import { useEffect, useState } from "react";
 import Button from "../ui/Button";
+import HeroStyled from "./Hero.styled";
 
-function Hero() {
-  // Membuat State movie
-  const [movie, setMovie] = useState("");
+function Hero(){
+    // destruct movie state
+    const [movie, setMovie] = useState("");
 
-  async function fetchMovie() {
-    const url = "https://www.omdbapi.com/?apikey=fcf50ae6&i=tt2975590";
-    // Melakukan Fetch data dari API omdb.
-    const response = await fetch(url);
-    const data = await response.json();
+    useEffect(async() => {
+        // melakukan effect: fetch data movie(api)
+        const response = await fetch("https://www.omdbapi.com/?apikey=d7497266&i=tt3896198");
 
-    // Update state movie dengan data movie (hasil fetch)
-    setMovie(data);
-  }
+        const data = await response.json();
+        setMovie(data);
+    }, []);
 
-  /**
-   * Menjalankan useEffect.
-   * Parameter kedua digunakan untuk custom lifecycle update.
-   * Jika state di dalam array berubah, maka jalankan useEffect lagi (lifecycle update).
-   * Jika state di dalam array kosong, maka jalankan sekali (lifecycle mount).
-   */
-  useEffect(fetchMovie, []);
-
-  // Tampilkan state movie.
-  console.log(movie);
-
-  return (
-    <div className={styles.container}>
-      <section className={styles.hero}>
-        <div className={styles.hero__left}>
-          <h2 className={styles.hero__title}>{movie.Title}</h2>
-          <h3 className={styles.hero__genre}>Genre: {movie.Genre}</h3>
-          <p className={styles.hero__description}>{movie.Plot}</p>
-          <Button>Watch</Button>
-        </div>
-        <div className="hero__right">
-          <img
-            className={styles.hero__image}
-            src={movie.Poster}
-            alt="placeholder"
-          />
-        </div>
-      </section>
-    </div>
-  );
+    return (
+        <HeroStyled>
+            <section>
+                <div className="hero__left">
+                    <h2>{movie.Title}</h2>
+                    <h3>Genre: {movie.Genre}</h3>
+                    <p>{movie.Plot}</p>
+                    <Button variant="secondary" size="lg">Watch Movie</Button>
+                </div>
+                <div className="hero__right">
+                    <img src={movie.Poster} alt="" />
+                </div>
+            </section>
+        </HeroStyled>
+    );
 }
 
 export default Hero;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,58 +1,37 @@
-/**
- * Import CSS Module Navbar.
- * Disimpan di object styles.
- */
-import styles from "./Navbar.module.css";
-// Import Link dari React Router
+// Import CSS Module
 import { Link } from "react-router-dom";
+import NavbarStyled from "./Navbar.styled";
 
-function Navbar() {
-  /**
-   * Menggunakan styles yang sudah diimport.
-   * Memanggilnya menggunakan expression.
-   */
-  return (
-    <div className={styles.container}>
-      <nav className={styles.navbar}>
-        <div>
-          <h1 className={styles.navbar__brand}>Movie App</h1>
-        </div>
-        <div>
-          {/*
-           * Membuat Link/Navigasi.
-           * Menggunakan Link Component dari React Router
-           */}
-          <ul className={styles.navbar__list}>
-            <li className={styles.navbar__item}>
-              <Link className={styles.navbar__link} to="/">
-                Home
-              </Link>
-            </li>
-            <li className={styles.navbar__item}>
-              <Link className={styles.navbar__link} to="/movie/create">
-                Add Movie
-              </Link>
-            </li>
-            <li className={styles.navbar__item}>
-              <Link className={styles.navbar__link} to="/movie/popular">
-                Popular
-              </Link>
-            </li>
-            <li className={styles.navbar__item}>
-              <Link className={styles.navbar__link} to="/movie/now">
-                Now Playing
-              </Link>
-            </li>
-            <li className={styles.navbar__item}>
-              <Link className={styles.navbar__link} to="/movie/top">
-                Top Rated
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </div>
-  );
+// buat component navbar
+function Navbar(){
+    return(
+        <NavbarStyled>
+            <nav>
+                <div>
+                    <h1>Movie App</h1>
+                </div>
+                <div>
+                    <ul>
+                        <li>
+                            <Link className="link" to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link className="link" to="/movie/create">Add Movie</Link>
+                        </li>
+                        <li>
+                            <Link className="link" to="/movie/popular">Popular</Link>
+                        </li>
+                        <li>
+                            <Link className="link" to="/movie/now">Now Playing</Link>
+                        </li>
+                        <li>
+                            <Link className="link" to="/movie/top">Top Rated</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </NavbarStyled>
+    );
 }
 
 export default Navbar;
